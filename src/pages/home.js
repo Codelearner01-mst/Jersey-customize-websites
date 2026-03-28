efc0f23d52f5ae8c509ca5c2ec90498c1a87e44d
Paul Code <kwabenap603@gmail.com>
2026-03-28 09:38:15 +0000

    Add whatsapp link
    Remove enquiry form
    
    Add image to the contact field

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,23 @@ import { toggleNav } from "../utils/navigations.js";
 import { closeNav } from "../utils/navigations.js";
 const navs = document.getElementById("navLinks");
 const hamburgerBtn = document.getElementById("hamburger");
+const whatsAppBtn = document.getElementById("wa-btn");
 
-document.getElementById("submitBtn").addEventListener("click", function () {
-  this.textContent = "✓ Sent! We'll be in touch.";
-  this.style.background = "#1a7a3c";
-  setTimeout(() => {
-    this.textContent = "Send Enquiry →";
-    this.style.background = "";
-  }, 3500);
+function whatsappLink() {
+  const whatsappMessage = `Hello, I am interested in your services. Could you please provide more information?`;
+  window.open(
+    `https://wa.me/233247857046?text=${encodeURIComponent(whatsappMessage)}`,
+    "_blank",
+  );
+}
+
+whatsAppBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const whatsappMessage = `Hello, I am interested in your services. Could you please provide more information?`;
+  window.open(
+    `https://wa.me/233247857046?text=${encodeURIComponent(whatsappMessage)}`,
+    "_blank",
+  );
 });
 
 hamburgerBtn.addEventListener("click", () => {
