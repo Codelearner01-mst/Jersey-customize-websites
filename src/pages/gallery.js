8517f75800dfd37b9115234b256d6294912c53fb
Paul Code <kwabenap603@gmail.com>
2026-03-29 17:12:40 +0000

    Add nav toggle to works page and gallery page
    Enhance hero section design

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,1 +1,14 @@
+import { toggleNav } from "../utils/navigations.js";
+import { closeNav } from "../utils/navigations.js";
+const navs = document.getElementById("navLinks");
+const hamburgerBtn = document.getElementById("hamburger");
 
+hamburgerBtn.addEventListener("click", () => {
+  toggleNav(navs, hamburgerBtn);
+});
+
+navs.addEventListener("click", (e) => {
+  if (e.target) {
+    closeNav(navs, hamburgerBtn);
+  }
+});
